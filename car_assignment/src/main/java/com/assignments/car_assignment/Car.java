1a219191148659d574cd38ce307e12b5cbf6bdb5
Uracilius <uracilmabiyev@gmail.com>
2023-03-26 23:57:56 -0400

    Tweaked car so it now accepts two cars with same models. Need to add a unique primary key ASAP

---
+++ car_assignment/src/main/java/com/assignments/car_assignment/Car.java
@@ -0,0 +1,52 @@
+package com.assignments.car_assignment;
+import javax.persistence.*;
+	@Entity
+	@Table(name = "cars",  schema = "public")
+	public class Car {
+	    
+		
+	    private String name;
+		
+		
+	    private String color;
+	    @Id
+	    private String dateOfManufacture;
+	    private String countryOfManufacture;
+	    
+	    public Car() {
+	    	super();
+	    }
+		public Car(String name, String color, String dateOfManufacture, String countryOfManufacture) {
+			super();
+			this.name = name;
+			this.color = color;
+			this.dateOfManufacture = dateOfManufacture;
+			this.countryOfManufacture = countryOfManufacture;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color) {
+			this.color = color;
+		}
+		public String getDateOfManufacture() {
+			return dateOfManufacture;
+		}
+		public void setDateOfManufacture(String dateOfManufacture) {
+			this.dateOfManufacture = dateOfManufacture;
+		}
+		public String getCountryOfManufacture() {
+			return countryOfManufacture;
+		}
+		public void setCountryOfManufacture(String countryOfManufacture) {
+			this.countryOfManufacture = countryOfManufacture;
+		}
+	 
+	}
